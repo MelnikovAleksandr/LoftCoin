d60eacaa3ff5458ae4d834aecdc373428ec94169
Your Name <melnikov150990@mail.ru>
2022-06-06 03:18:20 +0500

    Home work # 2 with additions

--- app/src/main/java/com/mas/loftcoin/ui/rates/RatesFragment.java
+++ app/src/main/java/com/mas/loftcoin/ui/rates/RatesFragment.java
@@ -9,7 +9,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.mas.loftcoin.R;
 import com.mas.loftcoin.databinding.FragmentRateBinding;
+import com.mas.loftcoin.ui.main.MainActivity;
 
 public class RatesFragment extends Fragment {
 
@@ -21,6 +23,7 @@ public class RatesFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final FragmentRateBinding binding = FragmentRateBinding.inflate(inflater, container, false);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.menu_item_rate_text2);
         return binding.getRoot();
     }
 }
--- app/src/main/java/com/mas/loftcoin/ui/wallets/WalletsFragment.java
+++ app/src/main/java/com/mas/loftcoin/ui/wallets/WalletsFragment.java
@@ -1,26 +1,103 @@
 package com.mas.loftcoin.ui.wallets;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.PagerSnapHelper;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.SnapHelper;
 
+import com.mas.loftcoin.R;
 import com.mas.loftcoin.databinding.FragmentWalletsBinding;
+import com.mas.loftcoin.ui.main.MainActivity;
 
 public class WalletsFragment extends Fragment {
+
+    private SnapHelper walletsSnapHelper;
 
     public WalletsFragment() {
 
     }
-
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         final FragmentWalletsBinding binding = FragmentWalletsBinding.inflate(inflater, container, false);
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(R.string.menu_item_accounts_text);
+        setHasOptionsMenu(true);
         return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        final FragmentWalletsBinding binding = FragmentWalletsBinding.bind(view);
+        walletsSnapHelper = new PagerSnapHelper();
+        walletsSnapHelper.attachToRecyclerView(binding.recyclerWallets);
+
+        final TypedValue value = new TypedValue();
+        view.getContext().getTheme().resolveAttribute(R.attr.walletCardWidth, value, true);
+        final DisplayMetrics displayMetrics = view.getContext().getResources().getDisplayMetrics();
+        final int padding = (int) (displayMetrics.widthPixels - value.getDimension(displayMetrics)) / 2;
+        binding.recyclerWallets.setPadding(padding, 0, padding, 0);
+        binding.recyclerWallets.setClipToPadding(false);
+
+        binding.recyclerWallets.addOnScrollListener(new CarouselScroller());
+
+        binding.recyclerWallets.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL, false));
+        binding.recyclerWallets.setAdapter(new WalletsAdapter());
+        binding.recyclerWallets.setVisibility(View.VISIBLE);
+        binding.walletCardInFragment.setVisibility(View.GONE);
+
+    }
+
+    @Override
+    public void onDestroyView() {
+        walletsSnapHelper.attachToRecyclerView(null);
+        super.onDestroyView();
+    }
+
+    private static class CarouselScroller extends RecyclerView.OnScrollListener {
+        @Override
+        public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+            final int centerX = (recyclerView.getLeft() + recyclerView.getRight()) / 2;
+            for (int i = 0; i < recyclerView.getChildCount(); ++i) {
+                final View child = recyclerView.getChildAt(i);
+                final int childCenterX = (child.getLeft() + child.getRight()) / 2;
+                final float childOffset = Math.abs(centerX - childCenterX) / (float) centerX;
+                float factor = (float) (Math.pow(0.8,childOffset));
+                child.setScaleX(factor);
+                child.setScaleY(factor);
+            }
+        }
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_wallets, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
